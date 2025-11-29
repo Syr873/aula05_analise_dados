0c74e6d3a1033c5450c44ccdb301af3e7d60e368
Syr873 <mtroi873@gmail.com>
2025-11-29 14:29:44 -0300

    Atividades e Desafios If-Else e Match Case

---
+++ aula05/Desafio_AV.py
@@ -0,0 +1,29 @@
+  #Calculo de media
+# print('Bem-vindo! \nEste programa irá calcular sua média e retornar sua situação')
+# p1 = float(input('Informe a nota da Prova 1: '))
+# p2 = float(input('Informe a nota da Prova 2: '))
+# p3 = float(input('Informe a nota da Prova 3: '))
+# p4 = float(input('Informe a nota da Prova 4: '))
+# media = (p1+p2+p3+p4)/4
+# if media>7:
+#     print(f'Média: {media} \nAPROVADO!')
+# elif 5<=media<=7:
+#     print(f'Média: {media} \nRECUPERAÇÃO!')
+# else:
+#     print(f'Média: {media} \nREPROVADO!')
+
+  #Numeros ordenados
+# print('Programa de ordem de numeros')
+# num1 = int(input('Digite o primeiro valor: '))
+# num2 = int(input('Digite o segundo valor: '))
+# num3 = int(input('Digite o terceiro valor: '))
+# if num1<num2 and num2<num3:
+#     print(f'{num1},{num2},{num3}')
+# elif num1>num2 and num2>num3:
+#     print(f'{num3},{num2},{num1}')
+# elif num2<num1 and num1<num3:
+#     print(f'{num2},{num1},{num3}')
+# elif num2<num3 and num3<num1:
+#     print(f'{num2},{num3},{num1}')
+# else:
+#     print(f'{num3},{num1},{num2}')
