ccaf0e21f02af251296d023c995433a49836fea4
Syr873 <mtroi873@gmail.com>
2025-11-29 15:30:15 -0300

    Desafios e Atividades If-Else e Match Case

---
+++ aula05/Desafio_PTD.py
@@ -0,0 +1,81 @@
+  #Região de procedência
+# codigo = int(input('Informe o código o produto: '))
+# if codigo == 1:
+#     print('Região Sul')
+# elif codigo == 2:
+#     print('Região Norte')
+# elif codigo == 3:
+#     print('Região Leste')
+# elif codigo == 4:
+#     print('Região Oeste')
+# elif codigo == 5 or codigo == 6:
+#     print('Região Nordeste')
+# elif codigo == 7 or codigo == 8 or codigo == 9:
+#     print('Região Sudeste')
+# elif codigo == 10:
+#     print('Região Centro-Oeste')
+# elif codigo == 11:
+#     print('Região  Noroeste')
+# else:
+#     print('Importado!')
+
+  #Forma de pagamento
+# opcao = input('Informe a forma de pagamento: ').lower()
+# if opcao == 'a vista':
+#     valor = float(input("Informe o valor: "))
+#     desconto = valor*(10/100)
+#     total = valor - desconto
+#     print(f'Valor: {valor} \nDesconto de 10%: {desconto:.2f} \nTotal: {total:.2f}')
+# elif opcao == 'pix':
+#     valor = float(input("Informe o valor: "))
+#     print(f'Sem desconto! \nTotal: {valor}')
+# elif opcao == 'debito':
+#     valor = float(input("Informe o valor: "))
+#     acrescimo = valor*(5/100)
+#     total = valor + acrescimo
+#     print(f'Valor: {valor:.2f} \nAcrescimo de 5%: {acrescimo:.2f} \nTotal: {total:.2f}')
+# elif opcao == 'credito':
+#     valor = float(input("Informe o valor: "))
+#     acrescimo = valor*(8/100)
+#     total = valor + acrescimo
+#     print(f'Valor: {valor:.2f} \nAcrescimo de 8%: {acrescimo:.2f} \nTotal: {total:.2f}')
+# elif opcao == 'cheque':
+#     valor = float(input("Informe o valor: "))
+#     acrescimo = valor*(12/100)
+#     total = valor + acrescimo
+#     print(f'Valor: {valor:.2f} \nAcrescimo de 12%: {acrescimo:.2f} \nTotal: {total:.2f}')
+# else:
+#     print('Opção inválida!')
+
+  #Situação do aluno
+# print('Bem-vindo! \nEste programa irá fornecer a situação do aluno')
+# nota1 = float(input('Informe a primeira nota: '))
+# nota2 = float(input('Informe a segunda nota: '))
+# print('*Nota: Se o aluno nâo realizou a avalição optativa, informe a nota = -1.')
+# nota_ao = float(input('Informe a nota da avaliação optativa: '))
+# if nota1<nota2:
+#    media = (nota_ao + nota2)/2
+#    if media >= 6:
+#       print(f'Media: {media} \nAprovado!')
+#    elif 3<=media<6:
+#       print(f'Media: {media} \nRecuperação!')
+#    else:
+#       print(f'Media: {media} \nReprovado!')
+# else:
+#    media = (nota1 + nota_ao)/2
+#    if media>=6:
+#       print(f'Media: {media} \nAprovado!')
+#    elif 3<=media<6:
+#       print(f'Media: {media} \nRecuperação!')
+#    else:
+#       print(f'Media: {media} \nReprovado!')
+
+
+
+ 
+
+
+
+
+
+
